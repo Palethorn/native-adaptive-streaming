eecb36021619ee7d638fea64ad8bc93be8def86b
David Cavar <wizzard405@gmail.com>
2017-10-26 21:12:30 +0200

    Formatting

--- player.js
+++ player.js
@@ -7,24 +7,24 @@ var dash;
 var loaded1 = loaded2 = false;
 
 function handleMediaError(hls) {
-  var now = performance.now();
-  if(!recoverDecodingErrorDate || (now - recoverDecodingErrorDate) > 3000) {
-    recoverDecodingErrorDate = performance.now();
-    var msg = "trying to recover from media Error ..."
-    console.warn(msg);
-    hls.recoverMediaError();
-  } else {
-    if(!recoverSwapAudioCodecDate || (now - recoverSwapAudioCodecDate) > 3000) {
-      recoverSwapAudioCodecDate = performance.now();
-      var msg = "trying to swap Audio Codec and recover from media Error ..."
-      console.warn(msg);
-      hls.swapAudioCodec();
-      hls.recoverMediaError();
+    var now = performance.now();
+    if(!recoverDecodingErrorDate || (now - recoverDecodingErrorDate) > 3000) {
+        recoverDecodingErrorDate = performance.now();
+        var msg = "trying to recover from media Error ..."
+        console.warn(msg);
+        hls.recoverMediaError();
     } else {
-      var msg = "cannot recover, last media error recovery failed ..."
-      console.error(msg);
+        if(!recoverSwapAudioCodecDate || (now - recoverSwapAudioCodecDate) > 3000) {
+            recoverSwapAudioCodecDate = performance.now();
+            var msg = "trying to swap Audio Codec and recover from media Error ..."
+            console.warn(msg);
+            hls.swapAudioCodec();
+            hls.recoverMediaError();
+        } else {
+            var msg = "cannot recover, last media error recovery failed ..."
+            console.error(msg);
+        }
     }
-  }
 }
 
 function reloadPlayer(e) {
@@ -77,44 +77,46 @@ function playMpd(url, la_url) {
     dash.attachSource(url);
 }
 
-function playM3u8(url){
-  var video = document.getElementById('video');
-  if(native){
-    video.classList.add("native_mode");
-    video.classList.remove("zoomed_mode");
-  } else {
-    video.classList.remove("native_mode");
-    video.classList.add("zoomed_mode");
-  }
-
-  if(hls){ hls.destroy(); }
-  hls = new Hls({debug:debug});
-  hls.on(Hls.Events.ERROR, function(event,data) {
-    var  msg = "Player error: " + data.type + " - " + data.details;
-    console.error(msg);
-    if(data.fatal) {
-      switch(data.type) {
-        case Hls.ErrorTypes.MEDIA_ERROR:
-          handleMediaError(hls);
-          break;
-        case Hls.ErrorTypes.NETWORK_ERROR:
-           console.error("network error ...");
-          break;
-        default:
-          console.error("unrecoverable error");
-          hls.destroy();
-          break;
-      }
+function playM3u8(url) {
+    var video = document.getElementById('video');
+    if(native){
+        video.classList.add("native_mode");
+        video.classList.remove("zoomed_mode");
+    } else {
+        video.classList.remove("native_mode");
+        video.classList.add("zoomed_mode");
     }
-   });
 
-  var m3u8Url = decodeURIComponent(url)
-  hls.loadSource(m3u8Url);
-  hls.attachMedia(video);
-  hls.on(Hls.Events.MANIFEST_PARSED,function() {
-    video.play();
-  });
-  document.title = url
+    if(hls) { hls.destroy(); }
+    hls = new Hls({debug:debug});
+
+    hls.on(Hls.Events.ERROR, function(event,data) {
+        var  msg = "Player error: " + data.type + " - " + data.details;
+
+        if(data.fatal) {
+            switch(data.type) {
+                case Hls.ErrorTypes.MEDIA_ERROR:
+                    handleMediaError(hls);
+                    break;
+                case Hls.ErrorTypes.NETWORK_ERROR:
+                    console.error("network error ...");
+                    break;
+                default:
+                    console.error("unrecoverable error");
+                    hls.destroy();
+                    break;
+              }
+        }
+    });
+
+    var m3u8Url = decodeURIComponent(url)
+    hls.loadSource(m3u8Url);
+    hls.attachMedia(video);
+    hls.on(Hls.Events.MANIFEST_PARSED,function() {
+        video.play();
+    });
+
+    document.title = url:
 }
 
 $.ajax('https://data.jsdelivr.com/v1/package/npm/dashjs', {
