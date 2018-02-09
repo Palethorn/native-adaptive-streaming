ec544867083af096bcdf84543428aa63af39bbfd
David Cavar <wizzard405@gmail.com>
2018-02-09 17:45:01 +0100

    Resolves #17: Extension no longer uses external script loading

--- player.js
+++ player.js
@@ -3,11 +3,8 @@
  */
 var hls;
 var debug;
-var hlsjsCurrentVersion = "0";
-var dashjsCurrentVersion = "0";
 var recoverDecodingErrorDate,recoverSwapAudioCodecDate;
 var dash;
-var loaded1 = loaded2 = false;
 
 function handleMediaError(hls) {
     var now = performance.now();
@@ -59,7 +56,7 @@ function playMpd(url, la_url) {
     var video_el = document.querySelector('video');
     video_element = video_el;
     dash = dashjs.MediaPlayer().create();
-
+    dash.getDebug().setLogToBrowserConsole(debug);
     dash.on(dashjs.MediaPlayer.events.ERROR, function (e) {
         console.error(e.error + ' : ' + e.event.message);
         if(e.error == 'key_session') {
@@ -122,39 +119,8 @@ function playM3u8(url) {
     document.title = url;
 }
 
-$.ajax('https://data.jsdelivr.com/v1/package/npm/dashjs', {
-    dataType: 'json',
-    success: function(data) {
-        loaded1 = true;
-        dashjsCurrentVersion = data.tags.latest;
-        restoreSettings();
-    },
-    error: function() {
-
-    }
-});
-
-$.ajax('https://data.jsdelivr.com/v1/package/npm/hls.js', {
-    dataType: 'json',
-    success: function(data) {
-        loaded2 = true;
-        hlsjsCurrentVersion = data.tags.latest;
-        restoreSettings();
-    },
-    error: function() {
-
-    }
-});
-
-
 function restoreSettings() {
-    if(!loaded1 || !loaded2) {
-        return;
-    }
-
     chrome.storage.local.get({
-        hlsjs: hlsjsCurrentVersion,
-        dashjs: dashjsCurrentVersion,
         debug: false,
         native: false
     }, function(settings) {
@@ -162,23 +128,11 @@ function restoreSettings() {
         native = settings.native;
         var url = window.location.href.split("#")[1];
 
-        var s1 = document.createElement('script');
-        var s2 = document.createElement('script');
-
-        s1.src = 'https://cdn.jsdelivr.net/npm/hls.js@' + settings.hlsjs + '/dist/hls.min.js';
-        (document.head || document.documentElement).appendChild(s1);
-
-        s2.src = 'https://cdn.jsdelivr.net/npm/dashjs@' + settings.dashjs + '/dist/dash.all.min.js';
-        (document.head || document.documentElement).appendChild(s2);
-
         if(url.indexOf(".mpd") > -1) {
-            s2.onload = function() { playMpd(url, null); };
+            playMpd(url, null);
         } else {
-            s1.onload = function() { playM3u8(url); };
+            playM3u8(url);
         }
-
-        $('head').append(s1);
-        $('head').append(s2);
     });
 }
 
@@ -192,3 +146,5 @@ $(window).bind('hashchange', function() {
         playM3u8(url);
     }
 });
+
+restoreSettings();
