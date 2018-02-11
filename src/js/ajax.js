dda84eb4fc34c531c016d8a03ec894c68e3c185d
David Cavar <wizzard405@gmail.com>
2018-02-11 18:30:49 +0100

    Bring back hlsjs and dashjs version selection for chrome

--- src/js/ajax.js
+++ src/js/ajax.js
@@ -2,12 +2,11 @@ function Ajax() {
     this.get = function(options) {
         this.options = options;
         this.xhttp = new XMLHttpRequest();
-        this.url = url;
         self = this;
 
         this.xhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
-                self.options.handler(this.responseText);
+                self.options.success(this.responseText);
             }
         };
 
--- src/js/options.js
+++ src/js/options.js
@@ -2,12 +2,30 @@
  * Modifications copyright (C) 2017 David Ćavar
  */
 
+// {% if config['target'] == 'chrome' %}
+
+var hlss_version = "0.8.9";
+var dashjs_version = "2.6.5";
+// {% endif %}
+
 function save_options() {
+  // {% if config['target'] == 'chrome' %}
+  
+  hlsjs_version = document.getElementById('hlsjsSel').value;
+  dashjs_version = document.getElementById('dashjsSel').value;
+  // {% endif %}
+
   var dbg = document.getElementById('cbDebug').checked;
   var ntv = document.getElementById('cbNative').checked;
   chrome.storage.local.set({
+    // {% if config['target'] == 'chrome' %}
+    hlsjs_version: hlsjs_version,
+    dashjs_version: dashjs_version,
+    // {% endif %}
+
     debug: dbg,
     native: ntv
+    
   }, function() {
     var status = document.getElementById('status');
     status.textContent = 'Options saved.';
@@ -19,6 +37,12 @@ function save_options() {
 
 function restore_options() {
   chrome.storage.local.get({
+    // {% if config['target'] == 'chrome' %}
+    
+    hlsjs_version: hls_current_version,
+    dashjs_version: dashjs_current_version,
+    // {% endif %}
+
     debug: false,
     native: false
   }, function(items) {
@@ -33,8 +57,68 @@ var loaded1 = false;
 var loaded2 = false;
 
 function attachEventListeners() {
+    // {% if config['target'] == 'chrome' %}
+    
+    if(!loaded1 || !loaded2) {
+        return;
+    }
+    // {% endif %}
+
     restore_options();
     document.getElementById('saveSettings').addEventListener('click', save_options);
 }
 
+// {% if config['target'] == 'chrome' %}
+
+var ajax1 = new Ajax();
+
+ajax1.get({
+    url: 'https://data.jsdelivr.com/v1/package/npm/dashjs',
+    success: function(data) {
+        loaded1 = true;
+        data = JSON.parse(data);
+        dashjs_version = data.tags.latest;
+
+        document.querySelector('#dashjsSel').innerHTML = '';
+    
+        for(var i = 0; i < data.versions.length; i++) {
+            var option = document.createElement('option');
+            option.value = data.versions[i];
+            option.innerText = data.versions[i];
+            document.querySelector('#dashjsSel').appendChild(option);
+        }
+
+        attachEventListeners();
+    }
+});
+
+var ajax2 = new Ajax();
+
+ajax2.get({
+    url: 'https://data.jsdelivr.com/v1/package/npm/hls.js',
+    success: function(data) {
+        loaded2 = true;
+        data = JSON.parse(data);
+        hls_current_version = data.tags.latest;
+
+        document.querySelector('#hlsjsSel').innerHTML = '';
+        for(var i = 0; i < data.versions.length; i++) {
+            var option = document.createElement('option');
+            option.value = data.versions[i];
+            option.innerText = data.versions[i];
+            document.querySelector('#hlsjsSel').appendChild(option);
+        }
+
+        attachEventListeners();
+    },
+    error: function() {
+
+    }
+});
+
+// {% elif config['target'] == 'firefox' %}
+
 attachEventListeners();
+
+// {% endif %}
+
