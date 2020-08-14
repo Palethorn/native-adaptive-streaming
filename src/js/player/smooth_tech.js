62cc58c970cccd110717e4112acc1a2dcd359d66
David Cavar <david.cavar@morescreens.net>
2020-08-14 11:32:56 +0200

    Issue #9 - Smooth streaming support using hasplayer.js

--- src/js/player/smooth_tech.js
+++ src/js/player/smooth_tech.js
@@ -1,10 +1,30 @@
 var SmoothTech = function(options) {
-    throw "Not implemented.";
-    
+    var self = this;
     this.options = options;
+
+    var stream = {
+        url: options.url
+    };
+
     this.player = new MediaPlayer();
-    this.player.init(this.options.video_element);
-    this.player.load(this.options.url);
+    this.player.init(options.video_element);
+    this.player.load(stream);
+    this.player.addEventListener("error", this.options.event_handler);
+    this.player.addEventListener("warning", this.options.event_handler);
+    this.player.addEventListener("cueEnter", this.options.event_handler);
+    this.player.addEventListener("cueExit", this.options.event_handler);
+    this.player.addEventListener("play_bitrate", this.options.event_handler);
+    this.player.addEventListener("download_bitrate", this.options.event_handler);
+    this.player.addEventListener("manifestUrlUpdate", this.options.event_handler);
+    this.player.addEventListener("metricAdded", this.options.event_handler);
+    this.player.addEventListener("metricChanged", this.options.event_handler);
+    this.player.addEventListener("bufferLevel_updated", this.options.event_handler);
+    this.player.addEventListener("state_changed", this.options.event_handler);
+    this.player.addEventListener("loadeddata", this.options.event_handler);
+    this.player.addEventListener("play", this.options.event_handler);
+    this.player.addEventListener("pause", this.options.event_handler);
+    this.player.addEventListener("timeupdate", this.options.event_handler);
+    this.player.addEventListener("volumechange", this.options.event_handler);
 
     this.getOptions = function() {
         return this.options;
@@ -20,13 +40,13 @@ var SmoothTech = function(options) {
 
     this.getQualities = function() {
         var u = this.player.getVideoBitrates();
+
         var bitrates = [];
 
         for(var i = 0; i < u.length; i++) {
             var b = {};
-            b.index = i
+            b.index = i;
             b.bitrate = u[i];
-            b.height = u[i] / 1000 + 'K';
             bitrates.push(b);
         }
 
@@ -34,20 +54,17 @@ var SmoothTech = function(options) {
     }
 
     this.setQuality = function(index) {
-        index = parseInt(index);
-
         if(index == -1) {
             this.player.setAutoSwitchQuality(true);
             return;
         }
 
         this.player.setAutoSwitchQuality(false);
-        this.player.setQualityFor("video", index);
+        this.player.setQualityFor('video', index);
     }
 
     this.destroy = function() {
         if(this.player != null) {
-            this.player.reset(0);
             this.player = null;
         }
     }
